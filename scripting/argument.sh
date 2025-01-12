2652320d5c68b4f253f8c3f1909786aeabb2c900
kp3073 <keyurpatel8288@gmail.com>
2025-01-12 16:05:30 -0500

    ss

--- scripting/argument.sh
+++ scripting/argument.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-
-aws --version >/dev/null 2>&1
-if [ $? -eq 0 ]; then
-    REGIONS=$@
-    for REGION in ${REGIONS}; do
-        aws ec2 describe-vpcs --region ${REGION} --query 'Vpcs[*].VpcId' --output text
-        echo "-----------------------"
-    done
+if [ $# -gt 0 ]; then
+    aws --version >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        REGIONS=$@
+        for REGION in ${REGIONS}; do
+            aws ec2 describe-vpcs --region ${REGION} --query 'Vpcs[*].VpcId' --output text
+            echo "-----------------------"
+        done
+    else
+        echo "vpc id not found"
+    fi
 else
-    echo "vpc id not found"
+    echo "Please provide region name"
 fi
