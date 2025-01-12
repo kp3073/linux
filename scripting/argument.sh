0775998d54c5274f6c7f3dda1510e7eb1a25440b
kp3073 <keyurpatel8288@gmail.com>
2025-01-12 15:55:56 -0500

    ss

--- scripting/argument.sh
+++ scripting/argument.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-REGIONS=$@
+
 aws --version >/dev/null 2>&1
 if [ $? -eq 0 ]; then
-    for REGION in REGIONS; do
-        aws ec2 describe-vpcs --region $REGION --query 'Vpcs[*].VpcId' --output text
+    REGIONS=$@
+    for REGION in ${REGIONS}; do
+        aws ec2 describe-vpcs --region ${REGION} --query 'Vpcs[*].VpcId' --output text
     done
 else
     echo "vpc id not found"
